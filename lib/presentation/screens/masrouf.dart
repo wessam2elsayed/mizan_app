f7cbbcafdc3255c1513a2d9cc8219df01e0a8c56
Wessam <wessam2elsayed82@gmail.com>
2026-01-18 03:06:52 +0200

    add masrouf screen

--- lib/presentation/screens/masrouf.dart
+++ lib/presentation/screens/masrouf.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:mizan_app/core/models/hive_model.dart';
 import 'package:mizan_app/core/strings/app_strings.dart';
 import 'package:mizan_app/core/theme/app_colors.dart';
+import 'package:mizan_app/presentation/widgets/card_widget.dart';
+import 'package:mizan_app/presentation/widgets/mizan_label.dart';
+
 
 class Masrouf extends StatefulWidget {
   const Masrouf({super.key});
@@ -11,47 +14,79 @@ class Masrouf extends StatefulWidget {
 }
 
 class _MasroufState extends State<Masrouf> {
-  final data =HiveModel().getData();
-   String username="";
-   String email="";
-   String salary="";
-   String balance="";
-   String selectedCountry="";
+  String salary="";
+  @override
+  void initState() {
+    super.initState();
+    final data = HiveModel().getData();
+    if(data.isNotEmpty){
+      setState(() {
+        salary=data.last.salary.toString();
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.babyBlue,
-      body:Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [         
-          ElevatedButton(onPressed: (){
-            for (var item in data) {
-            print("Username: ${item.name}");
-            print("Email: ${item.email}");
-            print("salary:${item.salary}");
-            print("balance: ${item.balance}");
-            print("country ${item.country}");
-            username = item.name; 
-          }
-          
-          },
-           child: Text(AppStrings.name)),
+      body:Column(
         
-           ElevatedButton(onPressed: (){
-            setState(() {
-    // هنا نعيد جلب البيانات وهي فارغة لتحديث الشاشة
-    var emptyData = HiveModel().getData(); 
-    print("List Length: ${emptyData.length}");
-  });
+        children: [    
+          MizanLabel(txt: AppStrings.masroufLabel,),
+          const SizedBox(height: 20,),
+          Container(width: 200,height: 100,
+          decoration: BoxDecoration(
+            color: AppColors.darkBlue,
+            borderRadius: BorderRadius.circular(25)
+          ),
+          child: Center(
+            child: Text(salary,
+            style:const TextStyle(color: AppColors.white,
+            fontSize: 20
+            ),),
+          ),),  
 
+          // ListView.builder(
+          //    scrollDirection: Axis.horizontal,
+          //  shrinkWrap: true,
+          //  itemCount: 3,
+          //   itemBuilder: (context,index)=>CardWidget())
+          
+                // ElevatedButton(onPressed: (){
+                //   for (var item in data) {
+                //   print("Username: ${item.name}");
+                //   print("Email: ${item.email}");                  
+                //   print("salary:${item.salary}");
+                //   print("balance: ${item.balance}");
+                //   print("country ${item.country}");
+                //   username = item.name; 
+                //   print(HiveModel().box?.values);
+                  
+
+                // }
         
+                // },
+                //  child: Text(AppStrings.name)),
       
-          },
-           child: Text(AppStrings.email)),
-        ],),
-      )
+        //          ElevatedButton(onPressed: (){
+        //           setState(() {
+        //             // HiveModel().clearAllData();
+                        // salary = "";   
+                        // username = "";
+        //             // لو عايزة امسح كل البيانات لكن بيفضل يعرض البيانات فى الاسم ويطلع طول القائمة صفر
+      
+        //   var emptyData = HiveModel().getData(); 
+        //   print("List Length: ${emptyData.length}");
+          
+        // });
+      
+      
+            
+        //         },
+        //          child: Text(AppStrings.email)),
+      ],)
     );
   }
 }
